ea69d9aecd5d6a3aac4bd0be404d10200157e998
Vishal Das <dvishal485@gmail.com>
2023-01-12 00:35:20 +0530

    improved qr scan page
    Signed-off-by: Vishal Das <dvishal485@gmail.com>

--- frontend/lib/Components/scan_qr.dart
+++ frontend/lib/Components/scan_qr.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:authonia/Components/add_manully.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
@@ -11,50 +12,66 @@ class ScanQR extends StatefulWidget {
 
 class _ScanScreenState extends State<ScanQR> {
   MobileScannerController cameraController = MobileScannerController();
+  bool perms = false;
+  late Timer timer;
+  @override
+  void initState() {
+    super.initState();
+    perms = !cameraController.isStarting;
+    timer = Timer.periodic(const Duration(seconds: 1), (_) {
+      setState(() {
+        perms = !cameraController.isStarting;
+        if (perms) timer.cancel();
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text('Scan QR Code'),
-          actions: [
-            IconButton(
-              color: Colors.white,
-              icon: ValueListenableBuilder(
-                valueListenable: cameraController.torchState,
-                builder: (context, state, child) {
-                  switch (state) {
-                    case TorchState.off:
-                      return const Icon(Icons.flash_off, color: Colors.grey);
-                    case TorchState.on:
-                      return const Icon(Icons.flash_on, color: Colors.yellow);
-                  }
-                },
-              ),
-              iconSize: 32.0,
-              onPressed: () {
-                cameraController.toggleTorch();
-                setState(() {});
-              },
-            ),
-          ],
+          actions: perms
+              ? []
+              : [
+                  IconButton(
+                    color: Colors.white,
+                    icon: const Icon(Icons.refresh),
+                    iconSize: 32.0,
+                    onPressed: () {
+                      setState(() {
+                        perms = !cameraController.isStarting;
+                      });
+                    },
+                  ),
+                ],
         ),
-        body: MobileScanner(
-            allowDuplicates: false,
-            onDetect: (barcode, args) {
-              if (barcode.rawValue != null &&
-                  barcode.rawValue!.startsWith('otpauth://totp/')) {
-                final String url = barcode.rawValue!;
-                debugPrint('Barcode found! $url');
-                Navigator.pop(context);
-                final uri = Uri.parse(url);
-                if (uri.scheme == 'otpauth' && uri.host == 'totp') {
-                  addManually(context,
-                      defUser:
-                          Uri.decodeQueryComponent(uri.path).split(':').last,
-                      defIssuer: uri.queryParameters['issuer'],
-                      defSecret: uri.queryParameters['secret']);
-                }
-              }
-            }));
+        body: !perms
+            ? const Center(
+                child: Text("Please give permission to camera first!"),
+              )
+            : MobileScanner(
+                allowDuplicates: false,
+                onDetect: (barcode, args) {
+                  if (barcode.rawValue != null &&
+                      barcode.rawValue!.startsWith('otpauth://totp/')) {
+                    final String url = barcode.rawValue!;
+                    debugPrint('Barcode found! $url');
+                    try {
+                      final uri = Uri.parse(url);
+                      if (uri.scheme == 'otpauth' && uri.host == 'totp') {
+                        Navigator.pop(context);
+                        addManually(context,
+                            defUser: Uri.decodeQueryComponent(uri.path)
+                                .split(':')
+                                .last,
+                            defIssuer: uri.queryParameters['issuer'],
+                            defSecret: uri.queryParameters['secret']);
+                      }
+                    } catch (e) {
+                      debugPrint(e.toString());
+                    }
+                  }
+                }));
   }
 }
