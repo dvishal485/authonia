1701bf0ea356ea727511eba845c75d6840e03281
Vishal Das <dvishal485@gmail.com>
2023-01-12 17:06:52 +0530

    fix qr scan not working on web
    Signed-off-by: Vishal Das <dvishal485@gmail.com>

--- frontend/lib/Components/scan_qr.dart
+++ frontend/lib/Components/scan_qr.dart
@@ -31,47 +31,78 @@ class _ScanScreenState extends State<ScanQR> {
     return Scaffold(
         appBar: AppBar(
           title: const Text('Scan QR Code'),
-          actions: perms
+          actions: !perms
               ? []
               : [
                   IconButton(
                     color: Colors.white,
-                    icon: const Icon(Icons.refresh),
+                    icon: ValueListenableBuilder(
+                      valueListenable: cameraController.torchState,
+                      builder: (context, state, child) {
+                        switch (state) {
+                          case TorchState.off:
+                            return const Icon(Icons.flash_off,
+                                color: Colors.grey);
+                          case TorchState.on:
+                            return const Icon(Icons.flash_on,
+                                color: Colors.yellow);
+                        }
+                      },
+                    ),
                     iconSize: 32.0,
                     onPressed: () {
-                      setState(() {
-                        perms = !cameraController.isStarting;
-                      });
+                      cameraController
+                          .toggleTorch()
+                          .then((_) => setState(() {}));
                     },
                   ),
                 ],
         ),
-        body: !perms
-            ? const Center(
-                child: Text("Please give permission to camera first!"),
-              )
-            : MobileScanner(
-                allowDuplicates: false,
-                onDetect: (barcode, args) {
-                  if (barcode.rawValue != null &&
-                      barcode.rawValue!.startsWith('otpauth://totp/')) {
-                    final String url = barcode.rawValue!;
-                    debugPrint('Barcode found! $url');
-                    try {
-                      final uri = Uri.parse(url);
-                      if (uri.scheme == 'otpauth' && uri.host == 'totp') {
-                        Navigator.pop(context);
-                        addManually(context,
-                            defUser: Uri.decodeQueryComponent(uri.path)
-                                .split(':')
-                                .last,
-                            defIssuer: uri.queryParameters['issuer'],
-                            defSecret: uri.queryParameters['secret']);
+        body: Column(
+          children: [
+            if (!perms)
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Center(child: CircularProgressIndicator()),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text('Please allow camera access to continue'),
+                    )
+                  ],
+                ),
+              ),
+            Expanded(
+              child: MobileScanner(
+                  fit: BoxFit.scaleDown,
+                  allowDuplicates: false,
+                  onDetect: (barcode, args) {
+                    if (barcode.rawValue != null &&
+                        barcode.rawValue!.startsWith('otpauth://totp/')) {
+                      final String url = barcode.rawValue!;
+                      debugPrint('Barcode found! $url');
+                      try {
+                        final uri = Uri.parse(url);
+                        if (uri.scheme == 'otpauth' && uri.host == 'totp') {
+                          cameraController.stop().then((_) {
+                            Navigator.pop(context);
+                            addManually(context,
+                                defUser: Uri.decodeQueryComponent(uri.path)
+                                    .split(':')
+                                    .last,
+                                defIssuer: uri.queryParameters['issuer'],
+                                defSecret: uri.queryParameters['secret']);
+                          });
+                        }
+                      } catch (e) {
+                        debugPrint(e.toString());
                       }
-                    } catch (e) {
-                      debugPrint(e.toString());
                     }
-                  }
-                }));
+                  }),
+            ),
+          ],
+        ));
   }
 }
